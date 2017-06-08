180b1aa360fdaeaf3f1ec742ac34c33536252796
Alex Tan Hong Pin <alextan@seekasia.com>
2017-06-08 18:52:46 +0800

    Create server.js

--- server.js
+++ server.js
@@ -2,60 +2,43 @@ const request = require('request')
 const debug = require('debug')('app:github')
 const fs = require('fs')
 
-const access_token = ''
 
-// min 2008
-const year = 2015
+const config = require('./config.js')
 
-// Fetch users from malaysia only
-const country = 'malaysia'
+const {
+  // Year in which the github account is created
+  year,
+  // Country where the github user resides
+  country,
+  // No of users fetched per call
+  per_page: perPage,
+  // Personal access token to reduce the rate-limit
+  access_token: accessToken
+} = config
 
-// Only 30 users are fetched from the github each time
-const perPage = 30
 
-function fetchUserRepos (username) {
-  request({
-    url: `https://api.github.com/users/${username}/repos`,
-    headers: {
-      'User-Agent': 'request'
-    }
-  }, (error, response, body) => {
-    console.log(response, body)
-    if (!error && response.statusCode === 200) {
-      const repos = JSON.parse(body)
-      console.log(repos.length)
-    } else {
-      console.log(error)
-    }
-  })
-}
-
-// Number of pages
+// A util helper to calculate the number of pages required for fetching all results
 function numberOfPages (totalPages, perPage) {
   return Math.floor(totalPages / perPage)
 }
 
+// Fetch the users data from a particular country
 function fetchUsersFromCountry (country, page) {
-  const url = `https://api.github.com/search/users?q=location:${country} created:${year}-01-01..${year + 1}-01-01&page=${page}`
- // &page=12
+  const startYear = year
+  const endYear = year + 1
+  const url = `https://api.github.com/search/users?q=location:${country} created:${startYear}-01-01..${endYear}-01-01&page=${page}`
 
   return new Promise((resolve, reject) => {
     request({
       url,
       headers: {
         'User-Agent': 'request',
-        'Authorization': `token ${access_token}`
-      }
-      // qs: {
-      //   client_id: ,
-      //   client_secret: ,
-      // },
-      // json: true
+        'Authorization': `token ${accessToken}`
+      },
+      json: true
     }, (error, response, body) => {
       if (!error && response.statusCode === 200) {
-        // const { total_count, items } = users
-        // body is a json string
-        resolve(JSON.parse(body))
+        resolve(body)
       } else {
         debug(`error fetching data user ${body}`)
         reject(error)
@@ -77,8 +60,7 @@ fetchUsersFromCountry(country, 1).then((data) => {
   })
   debug(`${mapPromises.length} items to fetch`)
 
-  return Promise.all(mapPromises)
-  .then((data) => {
+  return Promise.all(mapPromises).then((data) => {
     let users = []
 
     debug('Fetching users')
@@ -91,8 +73,6 @@ fetchUsersFromCountry(country, 1).then((data) => {
     if (success) {
       debug('Successfully wrote to file')
     }
-  }).catch((error) => {
-    debug(`Error resolving all: ${error}`)
   })
 }).catch((error) => {
   debug(`An error has occured: ${error}`)
